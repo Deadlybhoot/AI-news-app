ce56c848b374219031ff9e7ef9d321e6e37c6100
deadlybhoot <2041058@gcoej.ac.in>
2024-03-30 13:29:36 +0530

    after build commit

---
+++ build/precache-manifest.05eafa8244e3455668d14e3e8517d580.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384c0a4315bd66576580fdcb727d336c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "291bd0fc323827d860c8",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "610087ac7888b03fa2de",
+    "url": "/static/css/main.c8e3c5e2.chunk.css"
+  },
+  {
+    "revision": "291bd0fc323827d860c8",
+    "url": "/static/js/2.76fb5fa4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.76fb5fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "610087ac7888b03fa2de",
+    "url": "/static/js/main.f738226e.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "23e7c61f06d3ce6f7e96c0f0ee3f997d",
+    "url": "/static/media/hero4.23e7c61f.png"
+  }
+]);
